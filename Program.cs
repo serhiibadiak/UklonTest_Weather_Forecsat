57f9f05097bed4d3fdc6a215690f887fd41194e1
Serhii Badiak <sergio.badiak@gmail.com>
2021-07-02 16:11:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace UklonTest_Weather_Forecsat
+{
+    class Program
+    {
+        static public string Get_Forecst(string City)
+        {
+            string url = "https://goweather.herokuapp.com/weather/";
+            WebRequest request = WebRequest.Create(String.Concat(url, City));
+            request.Credentials = CredentialCache.DefaultCredentials;
+            try
+            {
+                WebResponse response = request.GetResponse();
+
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFromServer = reader.ReadToEnd();
+                    response.Close();
+                    Weather_Forecast current_forecast = new Weather_Forecast();
+                    current_forecast = JsonSerializer.Deserialize<Weather_Forecast>(responseFromServer);
+                    return current_forecast.temperature;
+                }
+            }
+            catch (WebException)
+            {
+                return "server  error";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kyiv weather is:");
+            Console.WriteLine(Get_Forecst("Kyiv"));
+            Console.WriteLine("Odesa weather is:");
+            Console.WriteLine(Get_Forecst("Odesa"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Weather_Forecast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UklonTest_Weather_Forecsat
+{
+    class Weather_Forecast
+    {
+            public string temperature { get; set; }
+            public string wind { get; set; }
+            public string description { get; set; }
+            public object forecast { get; set; }
+    }
+}
